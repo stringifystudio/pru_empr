c165609d3ce1515e48f1bf2552dd32e8d9ec4045
Stringify Studio <saebye24@gmail.com>
2025-05-20 11:28:40 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,11 @@ import Footer from './components/common/Footer';
 import HomePage from './pages/HomePage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import OrdersPage from './pages/OrdersPage';
 import LoginPage from './pages/LoginPage';
 import UpdatePasswordPage from './pages/UpdatePasswordPage';
 import ProfilePage from './pages/ProfilePage';
-
 
 function App() {
   return (
@@ -25,6 +26,8 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/product/:id" element={<ProductDetailPage />} />
                 <Route path="/cart" element={<CartPage />} />
+                <Route path="/checkout" element={<CheckoutPage />} />
+                <Route path="/orders" element={<OrdersPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/profile" element={<ProfilePage />} />
                 <Route path="/update-password" element={<UpdatePasswordPage />} />
