44571476d6900e0916d7fd322731d793e2bd1537
Stringify Studio <saebye24@gmail.com>
2025-05-30 11:31:57 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
+import { WishlistProvider } from './context/WishlistContext'; // <-- nuevo
+
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+
 import HomePage from './pages/HomePage';
 import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
@@ -16,32 +19,40 @@ import ProfilePage from './pages/ProfilePage';
 import CreateProductPage from './pages/CreateProductPage';
 import EditProductPage from './pages/EditProductPage';
 import CategoryPage from './pages/CategoryPage';
+import WishlistPage from './pages/WishlistPage'; // <-- si existe
+import AdminOrdersPage from './pages/AdminOrdersPage';
+import OrderDetailPage from './pages/OrderDetailPage'; // <-- nueva importación
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <CartProvider>
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/product/:id" element={<ProductDetailPage />} />
-                <Route path="/cart" element={<CartPage />} />
-                <Route path="/checkout" element={<CheckoutPage />} />
-                <Route path="/orders" element={<OrdersPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/update-password" element={<UpdatePasswordPage />} />
-                <Route path="/admin/create-product" element={<CreateProductPage />} />
-                <Route path="/admin/edit-product" element={<EditProductPage />} />
-                <Route path="/category/:categoryId" element={<CategoryPage />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-          <Toaster position="top-right" />
+          <WishlistProvider> {/* << Añadido */}
+            <div className="flex flex-col min-h-screen">
+              <Header />
+              <main className="flex-grow">
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/product/:id" element={<ProductDetailPage />} />
+                  <Route path="/cart" element={<CartPage />} />
+                  <Route path="/checkout" element={<CheckoutPage />} />
+                  <Route path="/orders" element={<OrdersPage />} />
+                  <Route path="/order/:id" element={<OrderDetailPage />} /> {/* <- nueva ruta para detalles del pedido */}
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/profile" element={<ProfilePage />} />
+                  <Route path="/update-password" element={<UpdatePasswordPage />} />
+                  <Route path="/category/:categoryId" element={<CategoryPage />} />
+                  <Route path="/wishlist" element={<WishlistPage />} /> {/* <- si lo usas */}
+                  <Route path="/admin/create-product" element={<CreateProductPage />} />
+                  <Route path="/admin/edit-product" element={<EditProductPage />} />
+                  <Route path="/admin/orders" element={<AdminOrdersPage />} /> {/* <- nueva ruta */}
+                </Routes>
+              </main>
+              <Footer />
+              <Toaster position="top-right" />
+            </div>
+          </WishlistProvider>
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
